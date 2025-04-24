3386c0918a3aab27cd59c94fe52091c341f77fa7
marisa <marisaviotti@gmail.com>
2025-04-24 17:03:37 -0300

    carrito CSS

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+//PRODUCTOS
+const productos = [
+    {
+        id: 1,
+        titulo: "Ser o No Ser",       
+        imagen: "./img/remeras/seronoser.png",
+        categoria: {
+            nombre: "remera",
+            id: 1,
+        },
+         precio: 15000,
+        },      
