eb2bf46d0754d58143a4eab3515ba9c4098ab55b
marisa <marisaviotti@gmail.com>
2025-04-24 19:48:05 -0300

    main js

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,185 @@
 //PRODUCTOS
 const productos = [
     {
-        id: 1,
+        id: "remera-01",
         titulo: "Ser o No Ser",       
-        imagen: "./img/remeras/seronoser.png",
+        imagen: "./img/remeras/remera1.png",
         categoria: {
-            nombre: "remera",
-            id: 1,
+            nombre: "remeras",
+            id: "remeras",
         },
          precio: 15000,
-        },      
+        },
+    {
+        id: "remera-02",
+        titulo: "I love art",
+        imagen: "./img/remeras/remera2.png",
+        categoria: {
+            nombre: "remeras",
+            id: "remeras",
+        },
+         precio: 15000,
+        },
+    {
+        id: "remera-03",
+        titulo: "Tengo ensayo",
+        imagen: "./img/remeras/remera3.png",
+        categoria: {
+            nombre: "remeras",
+            id: "remeras",
+        },
+         precio: 15000,
+        },
+    {
+        id: "remera-04",
+        titulo: "Vamos al teatro",
+        imagen: "./img/remeras/remera4.png",
+        categoria: {
+            nombre: "remeras",
+            id: "remeras",
+        },
+         precio: 15000,
+        },
+    {
+        id: "remera-05",
+        titulo: "Soy actriz",
+        imagen: "./img/remeras/remera5.png",
+        categoria: {
+            nombre: "remeras",
+            id: "remeras",
+        },
+         precio: 15000,
+        },
+    {
+        id: "remera-06",
+        titulo: "Soy actor",
+        imagen: "./img/remeras/remera6.png",
+        categoria: {
+            nombre: "remeras",
+            id: "remeras",
+        },
+         precio: 15000,
+        },
+    {
+        id: "buzo-01",
+        titulo: "To be or not to be",
+        imagen: "./img/buzos/buzo1.png",
+        categoria: {
+            nombre: "buzos",
+            id: "buzos",
+        },
+         precio: 25000,
+        },
+    {
+        id: "buzo-02",
+        titulo: "Mascaras",
+        imagen: "./img/buzos/buzo2.png",
+        categoria: {
+            nombre: "buzos",
+            id: "buzos",
+        },
+         precio: 25000,
+        },
+    {
+        id: "buzo-03",
+        titulo: "Love Theater",
+        imagen: "./img/buzos/buzo3.png",
+        categoria: {
+            nombre: "buzos",
+            id: "buzos",
+        },
+         precio: 25000,
+        },
+    {
+        id: "buzo-04",
+        titulo: "I have casting",
+        imagen: "./img/buzos/buzo4.png",
+        categoria: {
+            nombre: "buzos",
+            id: "buzos",
+        },
+         precio: 25000,
+        },
+    {
+        id: "buzo-05",
+        titulo: "I have a casting",
+        imagen: "./img/buzos/buzo5.png",
+        categoria: {
+            nombre: "buzos",
+            id: "buzos",
+        },
+         precio: 25000,
+        },
+    {
+        id: "buzo-06",
+        titulo: "I have a function",
+        imagen: "./img/buzos/buzo6.png",
+        categoria: {
+            nombre: "buzos",
+            id: "buzos",
+        },
+         precio: 25000,
+        },
+    {
+        id: "taza-01",
+        titulo: "The whole world is a stage",
+        imagen: "./img/tazas/taza1.png",
+        categoria: {
+            nombre: "tazas",
+            id: "tazas",
+        },
+         precio: 8000,
+        }
+    {
+        id: "taza-02",        
+        titulo: "Ser o no ser",
+        imagen: "./img/tazas/taza2.png",
+        categoria: {
+            nombre: "tazas",
+            id: "tazas",
+        },
+         precio: 8000,
+        }
+    {
+        id: "taza-03",        
+        titulo: "To be or not to be",
+        imagen: "./img/tazas/taza3.png",
+        categoria: {
+            nombre: "tazas",
+            id: "tazas",
+        },
+         precio: 8000,
+        }
+    {
+        id: "taza-04",        
+        titulo: "Shakespeare",
+        imagen: "./img/tazas/taza4.png",
+        categoria: {
+            nombre: "tazas",
+            id: "tazas",
+        },
+         precio: 8000,
+        }
+    {
+        id: "taza-05",        
+        titulo: "Ser o no ser",
+        imagen: "./img/tazas/taza5.png",
+        categoria: {
+            nombre: "tazas",
+            id: "tazas",
+        },
+         precio: 8000,
+        }
+    {
+        id: "taza-06",        
+        titulo: "I love art",
+        imagen: "./img/tazas/taza6.png",
+        categoria: {
+            nombre: "tazas",
+            id: "tazas",
+        },
+         precio: 8000,
+        }
+    ]
+  
+    
